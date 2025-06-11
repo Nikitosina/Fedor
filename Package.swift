f415c2e3ed377c53751b1df98c2152bac75f02f1
nikitarat <nikitarat1@gmail.com>
2025-06-11 11:17:07 +0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Fedor",
+    platforms: [ .macOS(.v14) ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+        .package(url: "telegram-bot-swift", from: "2.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "Fedor",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "TelegramBotSDK", package: "telegram-bot-swift"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/Fedor.swift
@@ -0,0 +1,70 @@
+import Hummingbird
+import Foundation
+import ArgumentParser
+@preconcurrency import TelegramBotSDK
+
+let tgBot = TelegramBot(token: "7869692240:AAGHGwd2B-wyXEqbdzF6c4UfJdXzelx2Nfc")
+let tgBotRouter = TelegramBotSDK.Router(bot: tgBot)
+let chatId: Int64 = 369172417
+
+@main
+struct HummingbirdArguments: AsyncParsableCommand {
+    @Option(name: .shortAndLong)
+    var hostname: String = "0.0.0.0"
+
+    @Option(name: .shortAndLong)
+    var port: Int = 8060
+
+    func run() async throws {
+        let app = buildApplication(
+            configuration: .init(
+                address: .hostname(self.hostname, port: self.port),
+                serverName: "Hummingbird"
+            )
+        )
+        tgBotRouter["start", .slashRequired] = { context in
+            print(context.chatId)
+            return true
+        }
+        Task {
+            do {
+                while let update = tgBot.nextUpdateSync() {
+                    try tgBotRouter.process(update: update)
+                }
+            } catch {}
+        }
+        try await app.runService()
+    }
+}
+
+func buildApplication(configuration: ApplicationConfiguration) -> some ApplicationProtocol {
+    let router = Router()
+
+    router.get("/") { _, _ in
+        return "Fedor API - Feedback collector"
+    }
+    router.post("/store_feedback", use: storeFeedback)
+
+    let app = Application(
+        router: router,
+        configuration: configuration
+    )
+    return app
+}
+
+func storeFeedback(_ request: Request, context: any RequestContext) async throws -> Response {
+    guard let userId = request.uri.queryParameters.get("user_id") else { return Response(status: .badRequest) }
+    guard let feedback = try? await request.decode(as: Feedback.self, context: context) else {
+        return Response(status: .badRequest)
+    }
+    let document = InputFile(filename: "logs.txt", data: feedback.logs.data(using: .utf8) ?? Data())
+    let messageContent = "UserID: \(userId)\nComment: \(feedback.comment)"
+
+    tgBot.sendDocumentSync(chatId: .chat(chatId), document: .inputFile(document), caption: messageContent)
+    return Response(status: .ok)
+}
+
+struct Feedback: Codable {
+    let comment: String
+    let logs: String
+}
